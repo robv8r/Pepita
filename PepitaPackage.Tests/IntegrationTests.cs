7030b933045d6b3e264e9d0550812f3458b80109
Simon Cropp <simon.cropp@gmail.com>
2014-04-04 11:12:57 +1100

    more testing

--- PepitaPackage.Tests/IntegrationTests.cs
+++ PepitaPackage.Tests/IntegrationTests.cs
@@ -25,24 +25,35 @@ public class IntegrationTests
         var outputFile = Path.Combine(packagePath, "Standard.1.0.0.0.nupkg");
         var expectedFile = Path.Combine(Environment.CurrentDirectory, "MyPackage.1.0.0.0.nupkg ");
 
+        VerifyPackagesAreTheSame(expectedFile, outputFile);
+    }
+
+    void VerifyPackagesAreTheSame(string expectedFile, string outputFile)
+    {
         using (var package1 = Package.Open(expectedFile, FileMode.Open))
         using (var package2 = Package.Open(outputFile, FileMode.Open))
         {
-            foreach (var part1 in package1.GetParts())
+            VerifyPartsAreTheSame(package1, package2);
+            VerifyPartsAreTheSame(package2, package1);
+        }
+    }
+
+    void VerifyPartsAreTheSame(Package package1, Package package2)
+    {
+        foreach (var part1 in package1.GetParts())
+        {
+            if (part1.Uri.OriginalString.EndsWith("psmdcp"))
             {
-                if (part1.Uri.OriginalString.EndsWith("psmdcp"))
-                {
-                    continue;
-                }
-                if (part1.Uri.OriginalString.EndsWith("rels"))
-                {
-                    continue;
-                }
-                var part2 = package2.GetPart(part1.Uri);
-                var hash1 = GetFileHash(part1);
-                var hash2 = GetFileHash(part2);
-                Assert.AreEqual(hash1, hash2, part1.Uri.OriginalString);
+                continue;
             }
+            if (part1.Uri.OriginalString.EndsWith("rels"))
+            {
+                continue;
+            }
+            var part2 = package2.GetPart(part1.Uri);
+            var hash1 = GetFileHash(part1);
+            var hash2 = GetFileHash(part2);
+            Assert.AreEqual(hash1, hash2, part1.Uri.OriginalString);
         }
     }
 
